8b0ee2b5d06032bf400956cb1fa66c904bf1c123
armancoding <arman.armament@gmail.com>
2022-11-07 20:01:37 -0500

    tab control added

--- JSUtils/animations.js
+++ JSUtils/animations.js
@@ -12,4 +12,19 @@ for (let i = 0; i < coll.length; i++) {
   });
 }
 
-//TABS TODO:
+//TABS
+
+let tabButtons = document.querySelectorAll(".headMenu button");
+let panels = document.querySelectorAll(".tabs")
+
+function showPanel(panelIndex)
+{
+    panels.forEach(function(node)
+    {
+        node.style.display="none";
+    });
+    panels[panelIndex].style.display="block";
+
+}
+
+showPanel(0);
